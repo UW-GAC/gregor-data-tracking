ac90ed9dcbf796413ff7c663a3b4fcc1d79c6635
Stephanie M. Gogarten <sdmorris@uw.edu>
2023-04-06 22:27:32 -0700

    update for U2

--- combined_workspace.R
+++ combined_workspace.R
@@ -9,10 +9,10 @@ options("GCLOUD_SDK_PATH"="~/Applications/google-cloud-sdk") # don't run in AnVI
 centers <- c("BCM", "CNH_I", "GSS", "BROAD", "UW_CRDR")
 center=rep(centers, times=c(rep(1,4),2))
 consent=c(rep("GRU", 5), "HMB")
-release <- "U1"
+release <- "U2"
 workspaces <- paste("AnVIL_GREGoR", center, release, consent, sep="_")
 namespace <- "anvil-datastorage"
-combined_workspace <- "GREGOR_COMBINED_CONSORTIUM_U1"
+combined_workspace <- paste0("GREGOR_COMBINED_CONSORTIUM_", release)
 combined_namespace <- "gregor-dcc"
 
 model_url <- "https://raw.githubusercontent.com/UW-GAC/gregor_data_models/main/GREGoR_data_model.json"
@@ -37,13 +37,16 @@ conversion_function <- function(cm) {
 }
 
 combine_tables <- function(table_name) {
+    #print(paste("table", table_name))
     lapply(workspaces, function(x) {
+        #print(paste("workspace", x))
         tables <- avtables(namespace=namespace, name=x)
         if (table_name %in% tables$table) {
             table <- avtable(table_name, namespace=namespace, name=x)
             # map columns to expected data types so they can be combined
             if (!grepl("_set$", t)) {
                 for (c in names(table)) {
+                    #print(paste("column", c))
                     FUN <- conversion_function(model[[table_name]][[c]])
                     table[[c]] <- FUN(table[[c]])
                 }
@@ -75,10 +78,10 @@ for (t in table_names) {
     table_list[[t]] <- outfile
 }
 
-json <- list("data_table_import.table_files" = table_list,
-             "data_table_import.model_url" = "https://raw.githubusercontent.com/UW-GAC/gregor_data_models/main/GREGoR_data_model.json",
-             "data_table_import.workspace_name" = combined_workspace,
-             "data_table_import.workspace_namespace" = combined_namespace,
-             "data_table_import.overwrite" = "true"
+json <- list("validate_data_model.table_files" = table_list,
+             "validate_data_model.model_url" = model_url,
+             "validate_data_model.workspace_name" = combined_workspace,
+             "validate_data_model.workspace_namespace" = combined_namespace,
+             "validate_data_model.import_tables" = "true"
 ) %>% toJSON(pretty=TRUE, auto_unbox=TRUE, unbox=TRUE)
-write(json, paste0(combined_workspace, "_data_table_import.json"))
+write(json, paste0(combined_workspace, "_validate_data_model.json"))
--- table_migration.R
+++ table_migration.R
@@ -9,11 +9,13 @@ options("GCLOUD_SDK_PATH"="~/Applications/google-cloud-sdk") # don't run in AnVI
 centers <- c("BCM", "CNH_I", "GSS", "BROAD", "UW_CRDR")
 center=rep(centers, times=c(rep(1,4),2))
 consent=c(rep("GRU", 5), "HMB")
-release1 <- "U1"
-release2 <- "U2"
+release1 <- "U2"
+release2 <- "U3"
 workspaces1 <- paste("AnVIL_GREGoR", center, release1, consent, sep="_")
 workspaces2 <- paste("AnVIL_GREGoR", center, release2, consent, sep="_")
 namespace <- "anvil-datastorage"
+
+model_url <- "https://raw.githubusercontent.com/UW-GAC/gregor_data_models/main/GREGoR_data_model.json"
 
 for (i in seq_along(workspaces1)) {
     tables <- avtables(namespace=namespace, name=workspaces1[i])
@@ -31,11 +33,11 @@ for (i in seq_along(workspaces1)) {
         unlink(tmpfile)
         table_list[[t]] <- outfile
     }
-    json <- list("data_table_import.table_files" = table_list,
-                 "data_table_import.model_url" = "https://raw.githubusercontent.com/UW-GAC/gregor_data_models/main/GREGoR_data_model.json",
-                 "data_table_import.workspace_name" = workspaces2[i],
-                 "data_table_import.workspace_namespace" = namespace,
-                 "data_table_import.overwrite" = "true"
+    json <- list("validate_data_model.table_files" = table_list,
+                 "validate_data_model.model_url" = model_url,
+                 "validate_data_model.workspace_name" = workspaces2[i],
+                 "validate_data_model.workspace_namespace" = namespace,
+                 "validate_data_model.import_tables" = "true"
     ) %>% toJSON(pretty=TRUE, auto_unbox=TRUE, unbox=TRUE)
-    write(json, paste0(workspaces2[i], "_data_table_import.json"))
+    write(json, paste0(workspaces2[i], "_validate_data_model.json"))
 }
