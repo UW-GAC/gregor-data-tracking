5baf82625979912c27801a13842ab52ca088ac0c
Stephanie Gogarten <sdmorris@uw.edu>
2023-08-04 21:09:39 +0000

    update for U04

--- table_migration.R
+++ table_migration.R
@@ -5,12 +5,11 @@ library(tidyr)
 library(readr)
 library(jsonlite)
 
-options("GCLOUD_SDK_PATH"="~/Applications/google-cloud-sdk") # don't run in AnVIL workspace
 centers <- c("BCM", "CNH_I", "GSS", "BROAD", "UW_CRDR")
-center=rep(centers, times=c(rep(1,4),2))
-consent=c(rep("GRU", 5), "HMB")
-release1 <- "U2"
-release2 <- "U3"
+center <- rep(centers, times=c(rep(1,3),rep(2,2)))
+consent <- c(rep("GRU", 3), rep(c("GRU", "HMB"), 2))
+release1 <- "U03"
+release2 <- "U04"
 workspaces1 <- paste("AnVIL_GREGoR", center, release1, consent, sep="_")
 workspaces2 <- paste("AnVIL_GREGoR", center, release2, consent, sep="_")
 namespace <- "anvil-datastorage"
@@ -33,11 +32,13 @@ for (i in seq_along(workspaces1)) {
         unlink(tmpfile)
         table_list[[t]] <- outfile
     }
-    json <- list("validate_data_model.table_files" = table_list,
-                 "validate_data_model.model_url" = model_url,
-                 "validate_data_model.workspace_name" = workspaces2[i],
-                 "validate_data_model.workspace_namespace" = namespace,
-                 "validate_data_model.import_tables" = "true"
+    json <- list("validate_gregor_model.table_files" = table_list,
+                 "validate_gregor_model.model_url" = model_url,
+                 "validate_gregor_model.workspace_name" = workspaces2[i],
+                 "validate_gregor_model.workspace_namespace" = namespace,
+                 "validate_gregor_model.import_tables" = "true",
+                 "validate_gregor_model.check_md5" = "false",
+                 "validate_gregor_model.check_vcf" = "false"
     ) %>% toJSON(pretty=TRUE, auto_unbox=TRUE, unbox=TRUE)
-    write(json, paste0(workspaces2[i], "_validate_data_model.json"))
+    write(json, paste0(workspaces2[i], "_valildate_gregor_model.json"))
 }
