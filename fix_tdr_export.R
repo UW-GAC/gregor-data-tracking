c4d8774dc72813c1d78027d1208d86ba54bf94cb
Stephanie Gogarten <sdmorris@uw.edu>
2023-09-19 18:06:07 +0000

    add script to put TDR-exported data tables back into GREGoR data model

--- fix_tdr_export.R
+++ fix_tdr_export.R
@@ -14,8 +14,11 @@ model_url <- "https://raw.githubusercontent.com/UW-GAC/gregor_data_models/main/G
 for (i in seq_along(workspaces)) {
   bucket <- avbucket(namespace=namespace, name=workspaces[i])
   tables <- avtables(namespace=namespace, name=workspaces[i])
+  file_inventory <- avtable("file_inventory", namespace=namespace, name=workspaces[i]) %>%
+    select(uri, file_ref)
   table_list <- list()
   for (t in tables$table) {
+    print(paste(workspaces[i], t))
     dat <- avtable(t, namespace=namespace, name=workspaces[i])
     
     # custom version of unnest_set_table since it looks different after TDR
@@ -33,13 +36,35 @@ for (i in seq_along(workspaces)) {
     unlink(tmpfile)
     
     # fix primary keys after TDR export
-    primary_key <- paste0(t, "_id")
-    old_key <- paste0("tdr:", primary_key)
-    if (old_key %in% names(dat)) {
+    if (t != "file_inventory") {
+      primary_key <- paste0(t, "_id")
+      old_key <- paste0("tdr:", primary_key)
       stopifnot(all(dat[[primary_key]] == dat[["datarepo_row_id"]]))
       dat2 <- dat %>%
         select(-!!primary_key) %>%
         rename(all_of(setNames(old_key, primary_key)))
+    } else {
+      dat2 <- dat
+    }
+    
+    # drop extraneous columns
+    dat2 <- dat2 %>%
+      select(-starts_with("import:"), -any_of("ingest_provenance"))
+    
+    # map drs to gs uri
+    for (col in names(dat2)[grepl("_file", names(dat2))]) {
+      if (any(!is.na(dat2[[col]]))) {
+        tmp <- left_join(dat2, file_inventory, by=setNames("file_ref", col))
+        if (any(!is.na(tmp[["uri"]]))) {
+          drs_col <- paste0(col, "_drs")
+          dat2 <- tmp %>%
+            rename(all_of(setNames(col, drs_col))) %>%
+            rename(all_of(setNames("uri", col)))
+        } else {
+          dat2 <- tmp %>%
+            select(-uri)
+        }
+      }
     }
     
     # write new table to workspace
@@ -53,7 +78,7 @@ for (i in seq_along(workspaces)) {
   }
   
   # JSON file to use for validation workflow
-  workflow_inputs_json(table_list = table_list, 
+  workflow_inputs_json(file_list = table_list, 
                        model_url = model_url, 
                        workspace = workspaces[i], 
                        namespace = namespace)
