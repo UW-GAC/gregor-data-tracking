96dfbd4cd8b51f9fc521a74a21e92a6d9c9cffa6
Stephanie Gogarten <sdmorris@uw.edu>
2023-11-16 18:18:39 +0000

    bug fix in table migration

--- table_migration.R
+++ table_migration.R
@@ -16,12 +16,14 @@ workspaces1 <- lapply(names(centers), function(consent)
 workspaces2 <- sub(cycle1, cycle2, workspaces1)
 namespace <- "anvil-datastorage"
 
-model_url <- "https://raw.githubusercontent.com/UW-GAC/gregor_data_models/main/GREGoR_data_model.json"
+model_url <- "https://raw.githubusercontent.com/UW-GAC/gregor_data_models/U05_tmp/GREGoR_data_model.json"
 
 for (i in seq_along(workspaces1)) {
+    message(workspaces1[i])
     tables <- avtables(namespace=namespace, name=workspaces1[i])
     table_list <- list()
     for (t in tables$table) {
+        message(t)
         dat <- avtable(t, namespace=namespace, name=workspaces1[i])
         if (grepl("_set$", t)) {
             dat <- unnest_set_table(dat)
@@ -29,12 +31,12 @@ for (i in seq_along(workspaces1)) {
         tmpfile <- tempfile()
         write_tsv(dat, tmpfile)
         bucket <- avbucket(namespace=namespace, name=workspaces2[i])
-        outfile <- paste0(bucket, "/", release1, "_data_tables/", workspaces1[i], "_", t, ".tsv")
+        outfile <- paste0(bucket, "/", cycle1, "_data_tables/", workspaces1[i], "_", t, ".tsv")
         gsutil_cp(tmpfile, outfile)
         unlink(tmpfile)
         table_list[[t]] <- outfile
     }
-    workflow_inputs_json(table_list = table_list, 
+    workflow_inputs_json(file_list = table_list, 
                          model_url = model_url, 
                          workspace = workspaces2[i], 
                          namespace = namespace)
