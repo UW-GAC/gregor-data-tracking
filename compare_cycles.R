94ba56e1f2f7954a3b521a7e2d26ea191983f40a
Stephanie Gogarten <sdmorris@uw.edu>
2023-11-28 22:29:38 +0000

    output for compare_cycles is broken up to make it easier to see differences for each entity

--- compare_cycles.R
+++ compare_cycles.R
@@ -14,15 +14,11 @@ workspaces1 <- lapply(names(centers), function(consent)
 workspaces2 <- sub(cycle1, cycle2, workspaces1)
 namespace <- "anvil-datastorage"
 
-allequal <- function(x, y) {
-  if (!all(is.na(x) == is.na(y))) return(FALSE)
-  return(all(x[!is.na(x)] == y[!is.na(y)]))
-}
-
 for (i in seq_along(workspaces1)) {
   message(workspaces1[i])
   tables <- avtables(namespace=namespace, name=workspaces1[i])
   tables_to_check <- tables$table[!(grepl("_set$", tables$table))]
+  table_list <- list()
   for (t in tables_to_check) {
     table1 <- avtable(t, namespace=namespace, name=workspaces1[i])
     table2 <- avtable(t, namespace=namespace, name=workspaces2[i])
@@ -35,18 +31,47 @@ for (i in seq_along(workspaces1)) {
       left_join(table1, by=entity_id, suffix=paste0(".", c(cycle2, cycle1)))
     if (nrow(dat) == 0) next
     message(t)
+    diff_list <- list()
     for (c in setdiff(names(table1), entity_id)) {
       col1 <- paste(c, cycle1, sep=".")
       col2 <- paste(c, cycle2, sep=".")
-      if (allequal(dat[[col1]], dat[[col2]])) {
-        dat <- dat %>%
-          select(-any_of(c(col1, col2)))
-      }
+      prob1 <- dat[is.na(dat[[col1]]) != is.na(dat[[col2]]), c(entity_id, col1, col2)]
+      prob2 <- dat[!is.na(dat[[col1]]) & !is.na(dat[[col2]]) & dat[[col1]] != dat[[col2]], c(entity_id, col1, col2)]
+      dat2 <- rbind(prob1, prob2)
+      if (nrow(dat2) > 0) diff_list[[c]] <- dat2
     }
-    dat <- dat[,c(entity_id, sort(setdiff(names(dat), entity_id)))]
+    ids <- unique(unlist(lapply(diff_list, function(x) x[[entity_id]])))
+    id_list <- list()
+    for (id in ids) {
+      id_tbl <- tibble(id)
+      names(id_tbl) <- entity_id
+      for (x in diff_list) {
+        tmp <- x[x[[entity_id]] == id,]
+        if (nrow(tmp) > 0) {
+          id_tbl <- left_join(id_tbl, tmp, by=entity_id)
+        }
+      }
+      id_list[[id]] <- id_tbl
+    }
     
-    outfile <- paste0(workspaces1[i], "_diff_", cycle2, "_", t, ".tsv")
-    write_tsv(dat, file=outfile)
+    col_sets <- lapply(id_list, names)
+    col_list <- list()
+    index <- 1
+    for (cols in unique(col_sets)) {
+      tmp_list <- list()
+      j <- 1
+      for (x in id_list) {
+        if (setequal(unlist(cols), names(x))) {
+          tmp_list[[j]] <- x
+          j <- j+1
+        }
+      }
+      col_list[[index]] <- bind_rows(tmp_list)
+      index <- index+1
+    }
+    
+    outfile <- paste0(workspaces1[i], "_diff_", cycle2, "_", t, ".txt")
+    writeLines(knitr::kable(col_list), outfile)
     gsutil_cp(outfile, paste0(avbucket(), "/", cycle2, "_QC/"))
     #gsutil_cp(outfile, paste0(avbucket(namespace=namespace, name=workspaces2[i]), "/post_upload_qc/"))
   }
