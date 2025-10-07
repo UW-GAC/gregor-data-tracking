74790090bcbad6e1a14bd060c78354ac201365b8
Stephanie Gogarten <sdmorris@uw.edu>
2025-10-07 17:45:11 +0000

    R04

--- workspace_management/fix_tdr_export.R
+++ workspace_management/fix_tdr_export.R
@@ -2,16 +2,16 @@ library(AnVIL)
 library(dplyr)
 library(tidyr)
 library(readr)
+library(stringr)
 source("workflow_inputs_json.R")
 
-# first: delete "workspace_attributes" table
-
-release <- "R03"
+release <- "R04"
 consent <- c("GRU", "HMB")
 workspaces <- paste("AnVIL_GREGoR", release, consent, sep="_")
 namespace <- "anvil-datastorage"
 
-model_url <- "https://raw.githubusercontent.com/UW-GAC/gregor_data_models/main/GREGoR_data_model.json"
+model_url <- "https://raw.githubusercontent.com/UW-GAC/gregor_data_models/0418b30badca6dcfeb74b655df6e1831f0d22bd2/GREGoR_data_model.json"
+#model_url <- "https://raw.githubusercontent.com/UW-GAC/gregor_data_models/main/GREGoR_data_model.json"
 model <- AnvilDataModels::json_to_dm(model_url)
 
 write_original_table <- function(dat, table_name, bucket) {
@@ -24,13 +24,10 @@ write_original_table <- function(dat, table_name, bucket) {
 
 for (i in seq_along(workspaces)) {
   bucket <- avstorage(namespace=namespace, name=workspaces[i])
-  tables <- avtables(namespace=namespace, name=workspaces[i])
-  file_inventory <- avtable("file_inventory", namespace=namespace, name=workspaces[i])
-  write_original_table(file_inventory, table_name="file_inventory", bucket=bucket)
-  file_inventory <- file_inventory %>%
-    select(uri, file_ref)
+  tables <- avtables(namespace=namespace, name=workspaces[i]) %>%
+    filter(table %in% names(model))
   table_list <- list()
-  for (t in setdiff(tables$table, "file_inventory")) {
+  for (t in tables$table) {
     print(paste(workspaces[i], t))
     dat <- avtable(t, namespace=namespace, name=workspaces[i])
     
@@ -56,7 +53,7 @@ for (i in seq_along(workspaces)) {
     dat2 <- dat2 %>%
       select(-starts_with("import:"), -any_of("ingest_provenance"))
     
-    ## TO-DO: implement fix for column names coerced to lower case and 5prime -> t_5prime
+    ## implement fix for column names coerced to lower case and 5prime -> t_5prime
     model_names <- names(model[[t]])
     dat_names <- names(dat2)
     fix_case <- model_names[tolower(model_names) %in% dat_names & !(model_names %in% dat_names)]
@@ -68,26 +65,9 @@ for (i in seq_along(workspaces)) {
       names(dat2)[names(dat2) == paste0("t_", n)] <- n
     }
     
-    # # map drs to gs uri
-    # for (col in names(dat2)[grepl("_file", names(dat2))]) {
-    #   if (any(!is.na(dat2[[col]]))) {
-    #     tmp <- left_join(dat2, file_inventory, by=setNames("file_ref", col))
-    #     if (any(!is.na(tmp[["uri"]]))) {
-    #       drs_col <- paste0(col, "_drs")
-    #       dat2 <- tmp %>%
-    #         rename(all_of(setNames(col, drs_col))) %>%
-    #         rename(all_of(setNames("uri", col)))
-    #     } else {
-    #       dat2 <- tmp %>%
-    #         select(-uri)
-    #     }
-    #   }
-    # }
-    
     # write new table to workspace
     tmpfile <- tempfile()
     write_tsv(dat2, tmpfile)
-    bucket <- avstorage(namespace=namespace, name=workspaces[i])
     outfile <- paste0(bucket, "/GREGoR_data_model_tables/", t, ".tsv")
     avcopy(tmpfile, outfile)
     unlink(tmpfile)
@@ -103,12 +83,12 @@ for (i in seq_along(workspaces)) {
 
 
 ## check
-
 prep_workspaces <- paste("AnVIL_GREGoR", release, "prep", consent, sep="_")
 for (i in seq_along(workspaces)) {
-  tables <- avtables(namespace=namespace, name=workspaces[i])
+  tables <- avtables(namespace=namespace, name=workspaces[i]) %>%
+    filter(table %in% names(model))
   
-  for (t in setdiff(tables$table, "file_inventory")) {
+  for (t in tables$table) {
     print(paste(workspaces[i], t))
     dat <- avtable(t, namespace=namespace, name=workspaces[i])
     prep <- avtable(t, namespace=namespace, name=prep_workspaces[i])
